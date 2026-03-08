adbf73215c9a18b90b97e8a2175dd91e678a23bf
parikshit-pb <papon46857@gmail.com>
2026-03-09 00:07:39 +0600

    added main js file

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const container = document.getElementById("issueContainer")
+
+function loadIssues(data) {
+    
+    container.innerHTML = ""
+    data.forEach(issue => {
+
+        const borderColor = issue.status === "open"
+        ? "border-green-500"
+        : "border-purple-500"
+
+        const card =
+        <div class="bg-white p-4 rounded shadow border-t-4 ${borderColor}">
+
+            <h2 class="font-bold text-lg ">${issue.title}</h2>
+
+            <p class="">${issue.description}</p>
+
+            <p>Status: ${issue.status}</p>
+
+            <p>Category: ${issue.category}</p>
+
+            <p>Author: ${issue.author}</p>
+
+            <p>Priority: ${issue.priority}</p>
+
+            <p>Label: ${issue.label}</p>
+
+            <p>${issue.createdAt}</p>
+            <p></p>
+
+        </div>
+        container.innerHTML += card
+    })
+}
+loadIssues(issues)
