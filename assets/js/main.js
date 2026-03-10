a46111a73c1787c30bc4ab4dbeb79b2a137988b5
parikshit-pb <papon46857@gmail.com>
2026-03-10 21:02:15 +0600

    added function implement

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,23 @@ function loadIssues(data) {
         ? "border-green-500"
         : "border-purple-500"
 
-        const card =
+        const card = `
+        
         <div class="bg-white p-4 rounded shadow border-t-4 ${borderColor}">
 
-            <h2 class="font-bold text-lg ">${issue.title}</h2>
+            <div class="flex justify-between mb-3">
+            
+            <div class="w-8 h-8 bg-green-100 rounded-full flex items-center justify-center "><i class="fa-solid fa-circle text-green-500 text-sm"></i>
+            </div>
 
-            <p class="">${issue.description}</p>
+            <span class="bg-red-100 font-semibold  text-red-500 text-xs px-5 py-2 rounded-full">
+            ${issue.priority}
+            </span>
+           
+            </div>
+            <h2 class="font-bold text-2xl mb-2">${issue.title}</h2>
+
+            <p class="text-gray-500 text-sm mb-4">${issue.description}</p>
 
             <p>Status: ${issue.status}</p>
 
@@ -27,9 +38,12 @@ function loadIssues(data) {
             <p>Label: ${issue.label}</p>
 
             <p>${issue.createdAt}</p>
-            <p></p>
+
+             <p></p>
 
         </div>
+        `
+
         container.innerHTML += card
     })
 }
@@ -52,3 +66,27 @@ function searchIssue() {
     )
     loadIssues(result)
 }
+
+function showLoader(){
+    const loader = document.getElementById("loader")
+    loader.classList.remove("hidden")
+    setTimeout(() => {
+        loader.classList.add("hidden")
+    },1000)
+}
+
+// function loadIssues(data) {
+//     showLoader()
+//     container.innerHTML = ""
+//     data.forEach(issue => {
+//         const borderColor = issue.status === "open"
+//         `
+//         <div class="bg-white p-4 rounded shadow">
+//         <h2>${issue.title}</h2>
+//         <p>${issue.description}</p>
+
+//         </div>
+//         `
+//         container.innerHTML += card
+//     })
+// }
